528dc0e804a9399a4552294850a4961ab6a8c50a
aitorpc <aitorpc@MacBook-Pro-de-Aitor.local>
2014-12-19 22:39:50 +0100

    Añadido enemigo

--- UBoat LEX/Juego.swift
+++ UBoat LEX/Juego.swift
@@ -17,6 +17,9 @@ class Juego: SKScene {
     var submarino = SKSpriteNode()
     var prisma = SKSpriteNode()
     
+    var enemigo = SKSpriteNode()
+    var moverEnemigo = SKAction()
+    
     
     var moverArriba = SKAction()
     var moverAbajo = SKAction()
@@ -28,49 +31,28 @@ class Juego: SKScene {
     
     override func didMoveToView(view: SKView) {
         
-        
         backgroundColor = UIColor.cyanColor()
-        
         
         heroe()
         prismaticos()
-        
-        
         crearEscenario()
-        
-        
+        //enemy()
     }
-    
-    
-    
     
     func heroe() {
         
         submarino = SKSpriteNode(imageNamed: "UBoat")
-//        submarino.xScale = 1
-//        submarino.yScale = 1
         
         submarino.setScale(0.6)
-        
         submarino.zPosition = 1
-        
         submarino.position = CGPointMake(80, 200)
         submarino.name = "heroe"
         
         addChild(submarino)
         
-        
-        
-        
         moverArriba = SKAction.moveByX(0, y: 20, duration: 0.2)
         moverAbajo = SKAction.moveByX(0, y: -20, duration: 0.2)
-
-        
     }
-    
-    
-    
-    
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         
@@ -80,28 +62,16 @@ class Juego: SKScene {
             let dondeTocamos = toke.locationInNode(self)
             
             if dondeTocamos.y > submarino.position.y {
-                
                 if submarino.position.y < 750 {
                     submarino.runAction(moverArriba)
                 }
-                
-                
             } else {
-                
                 if submarino.position.y > 50 {
-                    
                     submarino.runAction(moverAbajo)
                 }
             }
-            
         }
-        
-        
     }
-    
-    
-    
-    
     
     func prismaticos() {
         
@@ -111,36 +81,21 @@ class Juego: SKScene {
         prisma.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame))
         
         addChild(prisma)
-        
-        
-        
     }
-    
-    
-    
-    
-    
-    
-    
-    
+
     func crearEscenario() {
-        
         
         for var indice = 0; indice < 2; ++indice {
             
             let fondo = SKSpriteNode(imageNamed: "mar4")
             fondo.position = CGPoint(x: indice * Int(fondo.size.width), y: 0)
-            
             fondo.name = "fondo"
             fondo.zPosition = 0
             
             addChild(fondo)
 
         }
-        
-        
     }
-    
     
     func scrollHorizontal() {
         
@@ -159,17 +114,45 @@ class Juego: SKScene {
                 }
             }
         })
-        
-        
     }
     
+    func enemy() {
+        
+        var randomNumber = UInt(arc4random_uniform(UInt32(size.height)))
+        
+        enemigo = SKSpriteNode(imageNamed: "enemigo")
+        enemigo.setScale(0.3)
+        enemigo.zPosition = 1
+        enemigo.name = "enemigo"
+        
+        enemigo.position = CGPoint(
+            x: size.width + enemigo.size.width/2,
+            y: CGFloat(randomNumber))
+        
+        addChild(enemigo)
+        
+        moverEnemigo = SKAction.moveToX(size.width - enemigo.size.width * 2, duration: 2.0)
+        
+        //let mover = SKAction.moveToX(  -enemigo.size.width/2, duration: 2.0)
+        
+        //let eliminar = SKAction.removeFromParent()
+        
+        //enemigo.runAction(SKAction.sequence [ mover, eliminar ])
+    }
     
-    
+    func sacarEnemigo() {
+        for var i = 0; i < 5; ++i {
+            enemy()
+            enemigo.runAction(moverEnemigo)
+        }
+        if enemigo.position.x <= size.width - enemigo.size.width/2 {
+            enemigo.removeFromParent()
+        }
+    }
     
     override func update(currentTime: NSTimeInterval) {
         scrollHorizontal()
+        sacarEnemigo()
+        //enemigo.runAction(moverEnemigo)
     }
-    
-    
-    
 }
